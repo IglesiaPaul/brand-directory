f7e069c14ab4d4a741cc9e3eef26d4a1bc34139d
SHIGHMAN <me@pauliglesia.com>
2025-08-19 00:28:46 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1 style={{ fontSize: 24, fontWeight: 600 }}>Hemp’in Directory</h1>
+      <p>Go to <a href="/login">/login</a> to access the admin.</p>
+    </main>
+  );
+}
